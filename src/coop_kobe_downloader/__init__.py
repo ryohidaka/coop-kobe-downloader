3d08e1966e6ce72bb06ec9780d530c572fe9059c
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-06-22 16:40:47 +0900

    feat: ダウンロードディレクトリを指定できるように変更

--- src/coop_kobe_downloader/__init__.py
+++ src/coop_kobe_downloader/__init__.py
@@ -1,6 +1,9 @@
+import glob
 import os
+import time
 from coop_kobe_downloader.driver_helper import DriverHelper
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
@@ -9,19 +12,23 @@ from webdriver_manager.chrome import ChromeDriverManager
 def main():
     login_id = os.environ.get("LOGIN_ID")
     password = os.environ.get("PASSWORD")
+    download_dir = "."
 
-    downloader = CoopKobeDownloader(login_id, password)
+    downloader = CoopKobeDownloader(login_id, password, download_dir)
     downloader.download("2024062")
 
 
 class CoopKobeDownloader:
-    def __init__(self, login_id: str, password: str):
+    def __init__(self, login_id: str, password: str, download_dir: str = None):
         """
         CoopKobeDownloader クラスの初期化。
         """
         # 認証情報を定義
         self.login_id = login_id
         self.password = password
+
+        # ダウンロードディレクトリを定義
+        self.download_dir = download_dir
 
         # ChromeDriverを取得
         try:
@@ -59,7 +66,8 @@ class CoopKobeDownloader:
         """
         try:
             driver = webdriver.Chrome(
-                service=ChromeService(ChromeDriverManager().install())
+                service=ChromeService(ChromeDriverManager().install()),
+                options=self._get_chrome_options(),
             )
             return driver
         except Exception as e:
@@ -105,3 +113,50 @@ class CoopKobeDownloader:
             By.CLASS_NAME, "p-order-history-detail-control__dl-btn"
         )
         helper.click_button(download_button)
+
+        # ファイルダウンロード完了まで待機する
+        self._wait_for_download()
+
+    def _get_chrome_options(self) -> ChromeOptions:
+        """
+        Chromeのオプションを設定
+
+        Returns:
+        * 設定済みのChromeOptions
+        """
+
+        # ダウンロードディレクトリが指定されていない場合は、オプションを指定しない
+        if self.download_dir is None:
+            return None
+
+        os.makedirs(self.download_dir, exist_ok=True)
+
+        options = ChromeOptions()
+        options.add_experimental_option(
+            "prefs",
+            {
+                "profile.default_content_settings.popups": 0,
+                "download.default_directory": os.path.realpath(self.download_dir),
+            },
+        )
+
+        return options
+
+    def _wait_for_download(self):
+        """
+        ファイルダウンロード完了まで待機する
+        """
+        timeout_second = 10
+        for i in range(timeout_second):
+            downloaded_files = glob.glob(f"{os.path.realpath(self.download_dir)}\\*.*")
+
+            if any(
+                ".crdownload" not in os.path.splitext(file)[1]
+                for file in downloaded_files
+            ):
+                time.sleep(2)
+                return
+
+            # インデックスがtimeout_secondに達した場合にのみエラーを発生させる
+            if i == timeout_second - 1:
+                raise TimeoutError("ダウンロードが指定時間内に完了しませんでした。")
