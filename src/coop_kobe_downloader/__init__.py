ebcf42d6201a4d5495c8fdc863fc80266a6ba4b9
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-06-22 15:33:20 +0900

    feat: ログイン処理を追加

--- src/coop_kobe_downloader/__init__.py
+++ src/coop_kobe_downloader/__init__.py
@@ -1,6 +1,8 @@
 import os
+from coop_kobe_downloader.driver_helper import DriverHelper
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -28,12 +30,21 @@ class CoopKobeDownloader:
             print(f"ChromeDriverの取得に失敗しました: {e}")
             self.driver = None
 
+        # 宅配ページの共通URLを定義
+        self.base_url = "https://wwwckapp.coop-kobe.net"
+
     def download(self):
         """
         ダウンロード処理
         """
         if self.driver is None:
             print("WebDriverが存在しないため、ダウンロード処理をスキップします。")
+
+        # 宅配ページにログインする
+        self._login()
+
+        # ChromeDriverを閉じる
+        self.driver.quit()
 
     def _get_chrome_driver(self) -> webdriver.Chrome:
         """
@@ -51,3 +62,25 @@ class CoopKobeDownloader:
         except Exception as e:
             print(f"WebDriverのインスタンス作成に失敗しました: {e}")
             return None
+
+    def _login(self):
+        """
+        宅配ページにログインする
+        """
+        helper = DriverHelper(self.driver)
+
+        # ログイン画面に遷移
+        login_url = f"{self.base_url}/login.html"
+        helper.navigate_to_url(login_url)
+
+        # ログインIDを入力
+        input_login_id = helper.find_element(By.NAME, "LOGIN_ID")
+        helper.input_form(input_login_id, self.login_id)
+
+        # パスワードを入力
+        input_password = helper.find_element(By.NAME, "LOGIN_PASS")
+        helper.input_form(input_password, self.password)
+
+        # ログインボタンをクリック
+        login_button = helper.find_element(By.CLASS_NAME, "p-mypage-part-btn--action")
+        helper.click_button(login_button)
---
+++ src/coop_kobe_downloader/driver_helper.py
@@ -0,0 +1,65 @@
+from selenium.webdriver import Chrome as webdriver_Chrome
+import time
+
+
+class DriverHelper:
+    def __init__(self, driver: webdriver_Chrome):
+        """
+        DriverHelper クラスの初期化。
+
+        Parameters:
+        * driver: webdriverのインスタンス
+        * download_dir (str): ダウンロードディレクトリのパス
+        """
+        self.driver = driver
+
+    def navigate_to_url(self, url: str):
+        """
+        指定したURLに遷移
+
+        Parameters:
+        * url (str): 遷移先のURL
+        """
+        try:
+            self.driver.get(url)
+            time.sleep(5)
+        except Exception as e:
+            print(f"ページ遷移時にエラーが発生しました: {e}")
+
+    def find_element(self, type, selector: str):
+        """
+        指定した要素を探します。
+
+        Parameters:
+        * type: 要素の種類
+        * selector (str): 要素のセレクター
+
+        Returns:
+        * elements[0]: 要素
+        """
+        elements = self.driver.find_elements(type, selector)
+        if elements:
+            return elements[0]
+        else:
+            raise Exception(f"Element not found: {selector}")
+
+    def input_form(self, element, value: str):
+        """
+        指定した要素に値を入力します。
+
+        Parameters:
+        * element: 入力する要素
+        * value (str): 入力する値
+        """
+        element.send_keys(value)
+        time.sleep(1)
+
+    def click_button(self, button):
+        """
+        指定したボタンをクリックします。
+
+        Parameters:
+        * button: クリックするボタン
+        """
+        button.click()
+        time.sleep(2)
